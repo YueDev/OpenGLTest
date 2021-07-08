3e23da6b3fa966bd2b36c10fd6b595f5f0015d49
zhang yue <yuedev.cn@gmail.com>
2021-07-08 17:26:54 +0800

    add cmake debug

---
+++ cmake-build-debug/CMakeFiles/OpenGLTest.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/CPP/Demo/OpenGLTest/src/glad.c" "D:/CPP/Demo/OpenGLTest/cmake-build-debug/CMakeFiles/OpenGLTest.dir/src/glad.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/CPP/Demo/OpenGLTest/src/Shader.cpp" "D:/CPP/Demo/OpenGLTest/cmake-build-debug/CMakeFiles/OpenGLTest.dir/src/Shader.cpp.obj"
+  "D:/CPP/Demo/OpenGLTest/src/main.cpp" "D:/CPP/Demo/OpenGLTest/cmake-build-debug/CMakeFiles/OpenGLTest.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/OpenGLTest.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTest.dir/src/Shader.cpp.obj"
+  "CMakeFiles/OpenGLTest.dir/src/glad.c.obj"
+  "CMakeFiles/OpenGLTest.dir/src/main.cpp.obj"
+  "OpenGLTest.exe"
+  "OpenGLTest.exe.manifest"
+  "OpenGLTest.pdb"
+  "libOpenGLTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
